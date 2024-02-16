c4eae05448be10763a4bcf614faf1b6dc08f9c2c
James Lange <jaimeslange@gmail.com>
2024-02-16 12:51:59 +0100

    remix tutorial

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,62 @@
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from '@remix-run/react'
+import { useEffect, useState } from 'react'
+import {
+  LinksFunction,
+  LoaderFunctionArgs,
+  json,
+  redirect,
+} from '@remix-run/node'
+import appStylesHref from './app.css'
+import { createEmptyContact, getContacts } from './data'
+
+export const action = async () => {
+  const contact = await createEmptyContact()
+  return redirect(`/contacts/${contact.id}/edit`)
+}
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: appStylesHref },
+]
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
+  const contacts = await getContacts(q)
+
+  return json({ contacts, q })
+}
 
 export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
+  const submit = useSubmit()
+
+  const isSearching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q')
+
+  const [query, setQuery] = useState(q || '')
+
+  useEffect(() => {
+    const searchField = document.getElementById('q')
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || ''
+    }
+  }, [q])
+
   return (
     <html lang="en">
       <head>
@@ -20,13 +69,25 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form
+              id="search-form"
+              onChange={event => {
+                const isFirstSearch = q === null
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                })
+              }}
+              role="search"
+            >
               <input
                 id="q"
                 aria-label="Search contacts"
+                className={isSearching ? 'loading' : ''}
+                defaultValue={q || ''}
                 placeholder="Search"
                 type="search"
                 name="q"
+                value={query}
               />
               <div id="search-spinner" aria-hidden hidden={true} />
             </Form>
@@ -35,15 +96,42 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map(contact => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? 'active' : isPending ? 'pending' : ''
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}
+                      {contact.favorite ? <span>★</span> : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div
+          className={
+            navigation.state === 'loading' && isSearching ? 'loading' : ''
+          }
+          id="detail"
+        >
+          <Outlet />
         </div>
 
         <ScrollRestoration />
@@ -51,5 +139,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
